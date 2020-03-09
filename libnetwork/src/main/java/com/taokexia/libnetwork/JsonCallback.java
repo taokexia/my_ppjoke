0286078871ee80345e884a408e45e377b262dd98
taokexia <694198149@qq.com>
2020-03-09 23:06:30 +0800

    feat: add network lib

---
+++ libnetwork/src/main/java/com/taokexia/libnetwork/JsonCallback.java
@@ -0,0 +1,15 @@
+package com.taokexia.libnetwork;
+
+public class JsonCallback<T> {
+    public void onSuccess(ApiResponse<T> response) {
+
+    }
+
+    public void onError(ApiResponse<T> response) {
+
+    }
+
+    public void onCacheSuccess(ApiResponse<T> response) {
+
+    }
+}
